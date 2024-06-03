e71744c760026e5de504f6ad16d3760dce51de40
sis-shen <adongfa@gmail.com>
2024-06-03 23:54:17 +0800

    1

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -6,9 +6,35 @@
 #define LINE_SIZE 1024
 
 
+#define LEFT "["
+#define RIGHT "]"
+#define LABLE "#"
+
+char cline[LINE_SIZE];
+
+
+const char* getusername()
+{
+  return getenv("USER");
+}
+
+const char* gethostname()
+{
+  return getenv("HOSTNAME");
+}
+
+const char* getpwd()
+{
+  return getenv("PWD");
+}
+
+
+
 int main()
 {
 
-
+  printf(LEFT"%s@%s %s"RIGHT,getusername(),gethostname(),getpwd());
+  scanf("%s",cline);
+  printf("echo: %s\n",cline);
   return 0;
 }
