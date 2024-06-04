fb9542227449bc1b5aca92659276110b1b4a8e9e
sis-shen <adongfa@gmail.com>
2024-06-04 23:13:27 +0800

    1

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
 #define LINE_SIZE 1024
-
+#define ARGV_SIZE 1024
 
 #define LEFT "["
 #define RIGHT "]"
-#define LABLE "#"
+#define LABLE "# "
 
 char cline[LINE_SIZE];
+char* argv[ARGV_SIZE];
 
 
 const char* getusername()
@@ -25,16 +27,54 @@ const char* gethostname()
 
 const char* getpwd()
 {
-  return getenv("PWD");
+  const char*pwd = getenv("PWD");
+  int n = strlen(pwd);
+  while(n)//找最后一个/
+  {
+    if(pwd[n] == '/')break;
+    n--;
+  }
+  return (pwd+n+1);
 }
 
+void Splitcline(char*cline,char**argv,int* argv_n)
+{
+  memset(argv,0,sizeof(char*)*ARGV_SIZE);
+  int i = 1;
+  argv[0] = strtok(cline," ");
+  for(;i<ARGV_SIZE;i++)
+  {
+    argv[i] = strtok(NULL," ");
+    if(argv[i] == NULL) break;
+  }
 
+  *argv_n = i;
+}
+
+void Interact(char*cline,int size)
+{
+
+  printf(LEFT"%s@%s %s" RIGHT LABLE,getusername(),gethostname(),getpwd());
+  char*s = fgets(cline,size,stdin);
+  assert(s);
+  (void) s;//使用一下s，防止报警告
+  cline[strlen(cline)-1] = '\0';
+  printf("echo: %s\n",cline);
+}
 
 int main()
 {
+  int quit = 0;
+  while(!quit)
+  {
+    Interact(cline,sizeof(cline));
+    int argv_n;
+    Splitcline(cline,argv,&argv_n);
 
-  printf(LEFT"%s@%s %s"RIGHT,getusername(),gethostname(),getpwd());
-  scanf("%s",cline);
-  printf("echo: %s\n",cline);
+    
+
+
+  }
+
   return 0;
 }
