717763a7294201db1b5172cc4e03a0faf0e24630
sis-shen <adongfa@gmail.com>
2024-06-12 23:12:35 +0800

    1

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -7,7 +7,9 @@
 #include <sys/wait.h>
 
 #define LINE_SIZE 1024
-#define ARGV_SIZE 1024
+#define ARGV_SIZE 32 
+#define DLIM " \t"
+#define EXIT_CODE 446
 
 #define LEFT "["
 #define RIGHT "]"
@@ -15,6 +17,8 @@
 
 char cline[LINE_SIZE];
 char* argv[ARGV_SIZE];
+int last_code = 0;
+char pwd[LINE_SIZE];
 
 
 const char* getusername()
@@ -27,7 +31,7 @@ const char* gethostname()
   return getenv("HOSTNAME");
 }
 
-const char* getpwd()
+const char* showpwd()
 {
   const char*pwd = getenv("PWD");
   int n = strlen(pwd);
@@ -39,45 +43,70 @@ const char* getpwd()
   return (pwd+n+1);
 }
 
+void getpwd()
+{
+  getcwd(pwd,sizeof(pwd)-1);
+}
+
 void Splitcline(char*cline,char**argv,int* argv_n)
 {
-  memset(argv,0,sizeof(char*)*ARGV_SIZE);
+  memset(argv,0,sizeof(char*) * ARGV_SIZE);
   int i = 1;
-  argv[0] = strtok(cline," ");
-  for(;i<ARGV_SIZE;i++)
-  {
-    argv[i] = strtok(NULL," ");
-    if(argv[i] == NULL) break;
-  }
+  argv[0] = strtok(cline,DLIM);
+  if(argv[0])
+    while(argv[i++]=strtok(NULL,DLIM));
 
-  *argv_n = i;
+  *argv_n = i-1;
 }
 
 void Interact(char*cline,int size)
 {
 
-  printf(LEFT"%s@%s %s" RIGHT LABLE,getusername(),gethostname(),getpwd());
+  printf(LEFT"%s@%s %s" RIGHT LABLE,getusername(),gethostname(),showpwd());
   char*s = fgets(cline,size,stdin);
   assert(s);
   (void) s;//使用一下s，防止报警告
   cline[strlen(cline)-1] = '\0';
-  printf("echo: %s\n",cline);
+  //optrintf("echo: %s\n",cline);
 }
 
 void ExeternalCommand()
 {
   pid_t id =fork();
-  if(id == 0)
+  if(id < 0)
+  {
+    perror("fort");
+    return;
+  }
+  else  if(id == 0)
   {
     execvp(argv[0],argv+1);
     exit(446);
   }
   else 
   {
-    waitpid(id,NULL,0);
+    int status = 0;
+    pid_t rid = waitpid(id,&status,0);
+    if(rid == id)
+    {
+      last_code = WEXITSTATUS(status);
+    }
     return;
   }
 }
+
+int BuildCommand(char* _argv[],int _argv_n)//处理内建命令
+{
+  if(_argv_n == 2 && strcmp(_argv[0],"cd")== 0)
+  {
+    chdir(argv[1]);
+    getpwd();
+    sprintf(getenv("PWD"),"%s",pwd);
+    
+  }
+  return 0;
+}
+
 
 int main()
 {
@@ -88,7 +117,10 @@ int main()
     int argv_n;
     Splitcline(cline,argv,&argv_n);
 
-    ExeternalCommand();
+    if(argv_n == 0 )continue;
+    
+    int flag = BuildCommand(argv,argv_n);
+    if(!flag) ExeternalCommand();
 
 
   }
